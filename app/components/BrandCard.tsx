f7bc29a144609ac81fdbca1b07d7bb1f57e915c6
Eli Tamosauskas <eligijus@cast.ai>
2026-04-08 18:03:32 +0300

    fix brands card

--- app/components/BrandCard.tsx
+++ app/components/BrandCard.tsx
@@ -1,21 +1,23 @@
+import Image from "next/image";
+
 type Props = {
   name: string;
   description?: string | null;
   logo_url?: string | null;
 };
 
-export const BrandCard = ({ name, description, logo_url }: Props) => {
+export const BrandCard = ({ name, logo_url }: Props) => {
+  const src =
+    logo_url ??
+    "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp";
+
   return (
     <div className="card bg-base-100 shadow-sm w-full">
-      <figure>
-        <img
-          src={logo_url ?? "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"}
-          alt={name}
-        />
+      <figure className="relative h-32 bg-red-200">
+        <Image src={src} alt={name} fill className="object-cover" />
       </figure>
       <div className="card-body">
         <h2 className="card-title text-sm">{name}</h2>
-        {description && <p className="text-xs">{description}</p>}
       </div>
     </div>
   );
