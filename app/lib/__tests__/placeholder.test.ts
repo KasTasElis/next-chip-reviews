59c9892f1dd9b09860ef85e6ef2b51fbaa0b3e21
Eli Tamosauskas <eligijus@cast.ai>
2026-04-10 17:59:31 +0300

    testing ci

--- app/lib/__tests__/placeholder.test.ts
+++ app/lib/__tests__/placeholder.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from "vitest";
 
-describe('placeholder', () => {
-  it('passes', () => {
-    expect(true).toBe(true)
-  })
-})
+describe("placeholder", () => {
+  it("fails", () => {
+    expect(true).toBe(false);
+  });
+});
