edd230b0aa4c8b757213b574737b16320d46bef7
Eli Tamosauskas <eligijus@cast.ai>
2026-04-13 14:16:01 +0300

    fix half star rating display

--- app/components/StarRating.tsx
+++ app/components/StarRating.tsx
@@ -1,28 +1,40 @@
+import React from "react";
+
 export default function StarRating({
   rating = 0,
-  count,
+  count = 0,
 }: {
   rating?: number;
   count?: number;
 }) {
+  const displayRating = Math.round(rating * 2) / 2;
+
   return (
-    <div className="flex items-center gap-2">
-      <div className="rating rating-sm">
+    <div className="flex items-center gap-1">
+      <span className="text-md">{rating}</span>
+
+      <div className="rating rating-half rating-sm relative bottom-[1px]">
         {[1, 2, 3, 4, 5].map((v) => (
-          <input
-            key={v}
-            type="radio"
-            className="mask mask-star-2 bg-orange-400"
-            checked={rating === v}
-            readOnly
-            disabled
-          />
+          <React.Fragment key={v}>
+            <input
+              type="radio"
+              className="mask mask-star-2 mask-half-1 bg-orange-400"
+              checked={displayRating === v - 0.5}
+              readOnly
+              disabled
+            />
+            <input
+              type="radio"
+              className="mask mask-star-2 mask-half-2 bg-orange-400"
+              checked={displayRating === v}
+              readOnly
+              disabled
+            />
+          </React.Fragment>
         ))}
       </div>
 
-      <span className="text-sm opacity-60">
-        ({count === undefined ? "0" : count})
-      </span>
+      <span className="text-md opacity-50">({count})</span>
     </div>
   );
 }
